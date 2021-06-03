2d501e96ada2830e1d5d4ed0464bd2881863764c
Hafidh RK <hfrk.hafidh@gmail.com>
2021-06-03 12:07:57 +0900

    render [SECRET]

--- routes/secret.js
+++ routes/secret.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET secret. */
 router.get('/', function(req, res, next) {
-  res.redirect(process.env.SECRET);
+  res.render('index', { title: 'Express', SECRET: process.env.SECRET });
 });
 
 module.exports = router;
