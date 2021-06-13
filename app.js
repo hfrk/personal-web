ea47409a4a76eccbb572cdc0661cc352b6144dfa
Hafidh RK <hfrk.hafidh@gmail.com>
2021-06-13 18:17:37 +0900

    frontend revamp

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var echoRouter = require('./routes/echo');
 var secretRouter = require('./routes/secret');
+var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/echo', echoRouter);
 app.use('/secret', secretRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
