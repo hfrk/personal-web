abdb5ec3365ef021d5b22d95a947766d45bc2f3f
Hafidh RK <hfrk.hafidh@gmail.com>
2021-07-18 05:47:59 +0900

    canvas test

--- routes/canvas.js
+++ routes/canvas.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET canvas page. */
 router.get('/', function(req, res, next) {
-    res.status = 200;
+    res.status(200);
     res.sendFile(path.join(__dirname, '/public/canvas.html'));
 });
 
