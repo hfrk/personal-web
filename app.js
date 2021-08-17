773281281fe5e332c34806a1b369da8b5472a941
Hafidh RK <hfrk.hafidh@gmail.com>
2021-08-17 09:55:46 +0900

    Add scraper.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let fs = require('fs');
 
-var indexRouter = require('./routes/index');
-var echoRouter = require('./routes/echo');
-var secretRouter = require('./routes/secret');
-var testRouter = require('./routes/test');
-var blogRouter = require('./routes/blog');
+let indexRouter = require('./routes/index');
+let echoRouter = require('./routes/echo');
+let secretRouter = require('./routes/secret');
+let testRouter = require('./routes/test');
+let blogRouter = require('./routes/blog');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,12 +28,30 @@ app.use('/echo', echoRouter);
 app.use('/secret', secretRouter);
 app.use('/test', testRouter);
 app.use('/blog', blogRouter);
-app.use('/canvas', function(req, res, next) {
+app.use('/canvas', async (req, res, next) => {
     res.status(200);
     res.sendFile(path.join(__dirname, 'public/canvas.html'));
 });
-
-
+app.get('/jadwalkuliah', function(req, res, next) {
+    res.status(200);
+    res.sendFile(path.join(__dirname, 'not_public/jadwalkuliah.json'));
+});
+app.post('/jadwalkuliah', express.json(), async (req, res) => {
+    if (req.get("Authorization") === process.env.AUTH_JADWAL){
+        fs.writeFile(path.join(__dirname, 'not_public/jadwalkuliah.json'), JSON.stringify(req.body, null, 4), (err) => {
+            if (err) {
+                res.sendStatus(400);
+            }
+            else {
+                console.log("JSON data is saved.");
+            }
+        });
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(403);
+    }
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -50,7 +69,5 @@ app.use(function(err, req, res, next) {
       res.render('pages/layout', { title: `Error`, content: content });
   });
 });
-
-app.listen(8080);
 
 module.exports = app;
